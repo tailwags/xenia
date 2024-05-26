e399ec2c1456e67a69f11ca517c457faa4da3138
Matilde Morrone <contact@morrone.dev>
2024-05-26 15:53:06 +0200

    Add uname and exit_group syscalls

--- examples/uname.rs
+++ examples/uname.rs
@@ -1,18 +1,11 @@
-use core::{ffi::CStr, mem::MaybeUninit};
+use core::ffi::CStr;
 
-use linux_raw_sys::{general::__NR_uname, system::new_utsname};
-use xenia::syscall::syscall1;
+use xenia::uname;
 
 fn main() {
-    let mut uname = MaybeUninit::<new_utsname>::uninit();
+    let uname = uname();
 
-    unsafe {
-        let _ret = syscall1(__NR_uname as usize, uname.as_mut_ptr() as usize);
-    }
+    let release = unsafe { CStr::from_ptr(uname.release.as_ptr()) };
 
-    let uname = unsafe { uname.assume_init() };
-
-    let nodename = unsafe { CStr::from_ptr(uname.release.as_ptr()) };
-
-    dbg!(nodename);
+    dbg!(release);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,25 @@
 #![no_std]
 
+use core::{ffi::c_int, mem::MaybeUninit};
+
+use linux_raw_sys::{
+    general::{__NR_exit_group, __NR_uname},
+    system::new_utsname,
+};
+use syscall::{syscall1, syscall1_noreturn};
+
 pub mod errno;
 pub mod syscall;
+
+pub fn uname() -> new_utsname {
+    let mut uname = MaybeUninit::<new_utsname>::uninit();
+
+    unsafe {
+        syscall1(__NR_uname as usize, uname.as_mut_ptr() as usize);
+        uname.assume_init()
+    }
+}
+
+pub fn exit_group(exit_code: c_int) -> ! {
+    unsafe { syscall1_noreturn(__NR_exit_group as usize, exit_code as usize) }
+}
