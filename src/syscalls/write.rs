d72d682b0da6e93bc75d6870ea05a94053b7754f
Matilde Morrone <contact@morrone.dev>
2025-07-17 17:16:13 +0200

    Add error handling to write syscall

--- src/syscalls/write.rs
+++ src/syscalls/write.rs
@@ -1,8 +1,12 @@
-use crate::{Result, Syscall, fd::AsFd, syscall3_readonly};
+use crate::{Errno, Result, Syscall, fd::AsFd, syscall3_readonly};
 
 #[inline]
 pub fn write<Fd: AsFd>(fd: Fd, buf: &[u8]) -> Result<usize> {
     let res = unsafe { syscall3_readonly(Syscall::WRITE, fd, buf, buf.len()) };
+
+    if (-4095..0).contains(&(res as isize)) {
+        return Err(unsafe { Errno::from_raw(res as u16) });
+    }
 
     Ok(res)
 }
