df81dfba847af7351a6ee9818948c5607ec27398
Matilde Morrone <contact@morrone.dev>
2025-08-20 22:02:11 +0200

    Export rawfds for stdin, stdout and stderr

--- src/stdio.rs
+++ src/stdio.rs
@@ -1,7 +1,9 @@
-use linux_raw_sys::general::STDOUT_FILENO;
+use crate::fd::{BorrowedFd, RawFd};
 
-use crate::fd::BorrowedFd;
+pub const STDIN_FILENO: RawFd = linux_raw_sys::general::STDIN_FILENO as _;
+pub const STDOUT_FILENO: RawFd = linux_raw_sys::general::STDOUT_FILENO as _;
+pub const STDERR_FILENO: RawFd = linux_raw_sys::general::STDERR_FILENO as _;
 
 pub const fn stdout() -> BorrowedFd<'static> {
-    unsafe { BorrowedFd::borrow_raw(STDOUT_FILENO as _) }
+    unsafe { BorrowedFd::borrow_raw(STDOUT_FILENO) }
 }
