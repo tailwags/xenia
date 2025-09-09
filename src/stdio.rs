17e284487548aa95f37838c8b0d3d92b51aa11a6
Matilde Morrone <contact@morrone.dev>
2025-09-10 00:29:55 +0200

    Expose more stdio helpers

--- src/stdio.rs
+++ src/stdio.rs
@@ -4,6 +4,14 @@ pub const STDIN_FILENO: RawFd = linux_raw_sys::general::STDIN_FILENO as _;
 pub const STDOUT_FILENO: RawFd = linux_raw_sys::general::STDOUT_FILENO as _;
 pub const STDERR_FILENO: RawFd = linux_raw_sys::general::STDERR_FILENO as _;
 
+pub const fn stdin() -> BorrowedFd<'static> {
+    unsafe { BorrowedFd::borrow_raw(STDIN_FILENO) }
+}
+
 pub const fn stdout() -> BorrowedFd<'static> {
     unsafe { BorrowedFd::borrow_raw(STDOUT_FILENO) }
+}
+
+pub const fn stderr() -> BorrowedFd<'static> {
+    unsafe { BorrowedFd::borrow_raw(STDERR_FILENO) }
 }
