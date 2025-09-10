acfd7de1816a7b554512bfee4a281f36a0c45225
Matilde Morrone <contact@morrone.dev>
2025-09-10 05:01:38 +0200

    Add cwd helper

--- src/stdio.rs
+++ src/stdio.rs
@@ -3,6 +3,7 @@ use crate::fd::{BorrowedFd, RawFd};
 pub const STDIN_FILENO: RawFd = linux_raw_sys::general::STDIN_FILENO as _;
 pub const STDOUT_FILENO: RawFd = linux_raw_sys::general::STDOUT_FILENO as _;
 pub const STDERR_FILENO: RawFd = linux_raw_sys::general::STDERR_FILENO as _;
+pub const CWD: RawFd = linux_raw_sys::general::AT_FDCWD as _;
 
 pub const fn stdin() -> BorrowedFd<'static> {
     unsafe { BorrowedFd::borrow_raw(STDIN_FILENO) }
@@ -14,4 +15,8 @@ pub const fn stdout() -> BorrowedFd<'static> {
 
 pub const fn stderr() -> BorrowedFd<'static> {
     unsafe { BorrowedFd::borrow_raw(STDERR_FILENO) }
+}
+
+pub const fn cwd() -> BorrowedFd<'static> {
+    unsafe { BorrowedFd::borrow_raw(CWD) }
 }
