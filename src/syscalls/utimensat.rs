3d6d2efd0bc2fb1d9a8a5822c2f5c561bbe1eae5
Matilde Morrone <contact@morrone.dev>
2025-09-11 22:32:03 +0200

    Add support for empty paths in utimensat

--- src/syscalls/utimensat.rs
+++ src/syscalls/utimensat.rs
@@ -1,3 +1,5 @@
+use core::ffi::CStr;
+
 use crate::{
     AsCStr, AtFlags, Syscall, Timespec, Timestamps, fd::AsFd, syscall_result_unit,
     syscall4_readonly,
@@ -8,19 +10,31 @@ use linux_raw_sys::general::{AT_EMPTY_PATH, AT_SYMLINK_NOFOLLOW};
 #[inline]
 pub fn utimensat<Fd: AsFd, P: AsCStr>(
     fd: Fd,
-    path: P,
+    path: impl Into<Option<P>>,
     times: &Timestamps,
     flags: AtFlags,
 ) -> crate::Result<()> {
-    path.try_as_c_str(|path| {
-        syscall_result_unit(unsafe {
+    match path.into() {
+        Some(path) => path.try_as_c_str(|path| {
+            syscall_result_unit(unsafe {
+                syscall4_readonly(
+                    Syscall::UTIMENSAT,
+                    fd.as_fd(),
+                    path,
+                    &raw const *times,
+                    flags.bits(),
+                )
+            })
+        }),
+
+        None => syscall_result_unit(unsafe {
             syscall4_readonly(
                 Syscall::UTIMENSAT,
                 fd.as_fd(),
-                path,
+                None::<&CStr>,
                 &raw const *times,
                 flags.bits(),
             )
-        })
-    })
+        }),
+    }
 }
